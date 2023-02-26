4043ba1c67b2054e76880c41202e8cbca6bc8433
hafezmehr <hafezmehr@pm.me>
2023-02-26 14:49:16 +0330

    calculation tips added and the example removed

--- SocketServer/socketserver.py
+++ SocketServer/socketserver.py
@@ -1,5 +1,6 @@
-import socket, numpy as np
-from sklearn.linear_model import LinearRegression
+import socket
+import numpy as np
+from trading_bot.Bot.pivot_break import pivotBreakStrategy # You should remove this line. Look at the line 50 or README.md
 
 class socketserver:
     def __init__(self, address = '', port = ''):
@@ -18,7 +19,7 @@ class socketserver:
         # conn is the clientsocket. It's a socket object.
         # addr is the ip and port for data transmission, e.g. ('192.168.1.15', 49396)
         self.conn, self.addr = self.newSocket.accept() # Stablishing the connection
-        print('connected to', self.addr)
+        print('connected to ', self.addr)
 
         self.commdata = '' # For storing recieved data
         
@@ -28,39 +29,36 @@ class socketserver:
             # recv() recieves data in binary
             data = self.conn.recv(10000) # Data size in Bites (binary data)
             
-            #appending the recieved data to commdata
+            # Appending the recieved data to commdata
             self.commdata+=data.decode("utf-8") # Converting the data to UTF-8
             print(data) # Prints the recieved data
             
             if not data:
-                print("no data") # This usually will be printed on clientsocket timeouts
+                print("No data recieved from the client") # This usually will be printed on clientsocket timeouts
                 break # Breaks the while loop but the server will stay online
+
             # Sending the response to the client (in this case the MetaTrader Expert Advisor)
             # In this case we caculate a regressin on the recieved data and send the regression line position data
             # Data is converted from UTF-8 to binary using bytes()
-            self.conn.send(bytes(calcregr(self.commdata), "utf-8"))
+            self.conn.send(bytes(calc(self.commdata), "utf-8"))
             return self.commdata
         
     # Kills the socket server
     def __del__(self):
         self.newSocket.close()
 
-# Regression calculation using SciKit-Learn.
-# You can substitue this with any other calculation.
-# This part is copyied from this Persian tutorial by Masoomeh Karami: https://soodgah.com/shop/tutorials/P1066-metatrader5-python-socket-connection-course.html
-def calcregr(msg = ''):
-    chartdata = np.fromstring(msg, dtype=float, sep= ' ') # Data is seperated by a white space in this example
-    print(chartdata);
-    Y = np.array(chartdata).reshape(-1,1)
-    X = np.array(np.arange(len(chartdata))).reshape(-1,1)
-
-    lr = LinearRegression()
-    lr.fit(X, Y)
-    Y_pred = lr.predict(X)
-
-    P = Y_pred.astype(str).item(-1) + ' ' + Y_pred.astype(str).item(0)
-    print(P)
-    return str(P)
+# pivotBreakStrategy() is the calculation we needed to be performed.
+# You can substitue this with any other calculation you desire.
+# Examples are provided in the README.md
+def calc(msg = ''):
+    chartdata = np.fromstring(msg, dtype=float, sep= ' ') # Data is seperated by a white space (in client side's code).
+    print(chartdata) # prints the clean data
+    
+    # The following line is the calculation. You should remove this line and write your own task.
+    P = pivotBreakStrategy(chartdata)
+    
+    print(P) # prints the result
+    return str(P) #return the results in order to be sent back to the client
 
 # Creating a new sockek object with your ip and a port. It starts automatically.
 # The same ip and port will be used on the client side
